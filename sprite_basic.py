e5f4b7b36aecb59034c620cca3e20e7cf85fd8d0
Rajath Jain <rajath@amagi.com>
2019-06-25 00:18:03 +0530

    Collision detection for player and platform

--- sprite_basic.py
+++ sprite_basic.py
@@ -6,6 +6,12 @@ SIZE = WIDTH, HEIGHT = 1000, 700
 BG_COLOR = pygame.Color ('grey')
 FPS = 45
 SPRITE_IMAGES = 10
+WALK_SPEED = 3
+RUN_SPEED = 10
+
+def printRect (log_str, rect):
+    rect_str = str (rect.top) + ", " + str (rect.bottom) + ", " + str (rect.left) + ", " + str (rect.right)
+    print (log_str + " [top, bottom, left, right]: " + rect_str)
 
 class Direction (IntEnum):
     UP      = 1
@@ -21,7 +27,7 @@ class State (IntEnum):
     DEAD = 4
 
 class PlayerSprite (pygame.sprite.Sprite):
-    def __init__ (self):
+    def __init__ (self, platform_group, screen_rect):
         super (PlayerSprite, self).__init__ ()
         self.dead_w             = int (75*986/579)
         self.dead_h             = int (100*796/763)
@@ -46,6 +52,8 @@ class PlayerSprite (pygame.sprite.Sprite):
             self.images[State.JUMP].append (pygame.transform.scale (pygame.image.load (img_path_j), (self.alive_w, self.alive_h)))
             self.images[State.DEAD].append (pygame.transform.scale (pygame.image.load (img_path_d), (self.dead_w, self.dead_h)))
 
+        self.screen_rect = screen_rect
+        self.platform_group = platform_group
         self.index = 0.0
         self.state = State.IDLE
         self.image = self.images[self.state][int (self.index)]
@@ -76,35 +84,66 @@ class PlayerSprite (pygame.sprite.Sprite):
 )], int (self.w / 2), 0)
         return self.images[state][int (self.index)]
 
-    def update (self, floor_group):
+    def update (self):
         # Function to update the index and image
         if self.is_dead:
             self.index = (self.index + 0.5)
             if self.index >= len (self.images[self.state]):
                 self.index = len (self.images[self.state]) - 1
         elif self.is_jumping:
-            self.jump (floor_group)
+            self.jump ()
         else:
             self.index = (self.index + 0.5)
             self.gravity ()
             if self.index >= len (self.images[self.state]):
                 self.index = 0
 
-        self.get_collision (floor_group)
+        self.get_collision ()
+        rect = self.rect.clamp (self.screen_rect)
+        if rect.bottom < self.rect.bottom:
+            self.is_jumping = False
+            self.velocity = 0
+        self.rect = rect
         self.image  = self.get_image ()
         if not self.is_dead:
             self.state = State.IDLE
         #print (str (self.state) + " INSIDE else " + str (self.x_direction))
 
 ###############################################################################################
-    def get_collision (self, floor_group):
+    def is_player_at_sides (self, p_rect, c_rect):
+        ret = [False, False]
+        if (p_rect.top >= c_rect.top + 10 and p_rect.top < c_rect.bottom - 10) or \
+                (p_rect.bottom <= c_rect.bottom - 10 and p_rect.bottom > c_rect.top + 10):
+            ret[0] = True
+        if (p_rect.left >= c_rect.left + 10 and p_rect.left < c_rect.right - 10) or \
+                (p_rect.right <= c_rect.right - 10 and p_rect.right > c_rect.left + 10):
+            ret[1] = True
+        return ret
+
+    def get_collision (self):
         min_y = self.rect.bottom
-        for collided_sprite in pygame.sprite.spritecollide (self, floor_group, False):
-            if collided_sprite.rect.y < min_y and self.rect.bottom > collided_sprite.rect.y:
-                min_y = collided_sprite.rect.y
-            self.velocity = 0
-            print (str (min_y) + " " + str (self.rect.bottom))
-        self.rect.bottom = min_y
+        printRect ("Before collision player rect:", self.rect)
+        for collided_sprite in pygame.sprite.spritecollide (self, self.platform_group, False):
+            ret = self.is_player_at_sides (self.rect, collided_sprite.rect)
+            if ret[0]:
+                if self.rect.left < collided_sprite.rect.left and self.rect.right >= collided_sprite.rect.left:    # Left of platform
+                    self.rect.right = collided_sprite.rect.left 
+                    print ("Present at left side")
+                elif self.rect.right > collided_sprite.rect.right and self.rect.left <= collided_sprite.rect.right:  # Right of platform
+                    self.rect.left = collided_sprite.rect.right
+                    print ("Present at right side")
+            if ret[1]:
+                if self.rect.bottom > collided_sprite.rect.bottom and self.rect.top <= collided_sprite.rect.bottom:    # Bottom of platform
+                    self.rect.top = collided_sprite.rect.bottom
+                    print ("Present at bottom")
+                elif self.rect.top < collided_sprite.rect.top and self.rect.bottom >= collided_sprite.rect.top:  # Top of platform
+                    print ("Present at top")
+                    self.rect.bottom = collided_sprite.rect.top
+                    self.velocity = 0
+                    self.is_jumping = False
+
+            printRect ("Collision rect", collided_sprite.rect)
+        printRect ("After collision player rect:", self.rect)
 ################################################################################################
 
     def move (self, dx=0, dy=0):
@@ -137,7 +176,7 @@ class PlayerSprite (pygame.sprite.Sprite):
             self.velocity = 30
         self.move (0, int (self.velocity))
 
-    def jump (self, floor_group):
+    def jump (self):
         if not self.is_dead:
             if not self.is_jumping:
                 self.is_jumping = True
@@ -155,21 +194,21 @@ class PlayerSprite (pygame.sprite.Sprite):
 
                 self.velocity -= int (self.acceleration * self.unit_time)
                 self.rect.y -= self.velocity
-                min_y = self.rect.y
-                if self.rect.bottom >= HEIGHT:
-                    self.rect.bottom = HEIGHT - 1
-                    self.is_jumping = False
-                    self.velocity = 0
+                #if self.rect.bottom >= HEIGHT:
+                #    self.rect.bottom = HEIGHT - 1
+                #    self.is_jumping = False
+                #    self.velocity = 0
                     #self.dead ()
                 #print (str (self.rect.y) + " "  + str (min_y))
 
-class BoxSprite (pygame.sprite.Sprite):
+class PlatformSprite (pygame.sprite.Sprite):
     def __init__ (self):
-        super (BoxSprite, self).__init__ ()
-        self.image = pygame.Surface([WIDTH - 500, 30])
+        super (PlatformSprite, self).__init__ ()
+        self.image = pygame.Surface([WIDTH - 500, HEIGHT - 50])
         self.image.fill (pygame.Color ("brown"))
         self.rect = self.image.get_rect ()
-        self.rect = pygame.Rect(0, 500, WIDTH - 500, 30)
+        self.rect.x = 0
+        self.rect.y = 620
 
 def main ():
     pygame.init ()
@@ -177,10 +216,10 @@ def main ():
     screen = pygame.display.set_mode (SIZE)
     screen_rect = screen.get_rect ()
 
-    player = PlayerSprite ()
-    ground = BoxSprite ()
-    the_group = pygame.sprite.Group (player)
-    the_floor = pygame.sprite.Group (ground)
+    ground = PlatformSprite ()
+    platforms = pygame.sprite.Group (ground)
+    player = PlayerSprite (platforms, screen_rect)
+    actors = pygame.sprite.Group (player)
 
     clock = pygame.time.Clock ()
 
@@ -195,7 +234,7 @@ def main ():
                 player.dead ()
                 break
             if (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
-                player.jump (the_floor)
+                player.jump ()
                 #print ("Jumping here " + str (int (Direction.UP)))
 
         speed = 0
@@ -204,31 +243,28 @@ def main ():
             if keys[pygame.K_LEFT]:
                 player.x_direction = Direction.LEFT
                 player.state = State.WALK
-                speed = -3
+                speed = -WALK_SPEED
                 if pygame.key.get_mods () & pygame.KMOD_SHIFT:
-                    speed = -10
+                    speed = -RUN_SPEED
                     player.state = State.RUN
                 #player.move (speed, 0)
             elif keys[pygame.K_RIGHT]:
                 player.x_direction = Direction.RIGHT
                 player.state = State.WALK
-                speed = 3
+                speed = WALK_SPEED
                 if pygame.key.get_mods () & pygame.KMOD_SHIFT:
-                    speed = 10
+                    speed = RUN_SPEED
                     player.state = State.RUN
                 #player.move (speed, 0)
+
         player.move (speed, 0)
 
         screen.fill (BG_COLOR)
 
-        the_group.update (the_floor)
+        actors.update ()
 
-        #print ("before " + str (player.rect))
-        player.rect.clamp_ip (screen_rect)
-        #print ("after " + str (player.rect))
-
-        the_group.draw (screen)
-        the_floor.draw (screen)
+        actors.draw (screen)
+        platforms.draw (screen)
 
         pygame.display.update ()
 
