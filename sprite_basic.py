5a1d5abaa94a0a27414cf826d25fcb95cfd393d4
Rajath Jain <rajath@amagi.com>
2019-06-30 01:57:53 +0530

    Added coins to the game

--- sprite_basic.py
+++ sprite_basic.py
@@ -8,7 +8,7 @@ FPS = 45
 SPRITE_IMAGES = 10
 WALK_SPEED = 4
 ZOMBIE_SPEED = 1
-RUN_SPEED = 10
+RUN_SPEED = 8
 
 def printRect (log_str, rect):
     rect_str = str (rect.top) + ", " + str (rect.bottom) + ", " + str (rect.left) + ", " + str (rect.right)
@@ -28,7 +28,7 @@ class State (IntEnum):
     DEAD = 4
 
 class PlayerSprite (pygame.sprite.Sprite):
-    def __init__ (self, pos, platform_group, spike_group, zombie_group, screen_rect):
+    def __init__ (self, pos, platform_group, spike_group, zombie_group, coin_group, screen_rect):
         super (PlayerSprite, self).__init__ ()
         self.dead_w             = int (75*986/579)
         self.dead_h             = int (100*796/763)
@@ -57,6 +57,7 @@ class PlayerSprite (pygame.sprite.Sprite):
         self.zombie_group = zombie_group
         self.platform_group = platform_group
         self.spike_group = spike_group
+        self.coin_group = coin_group
         self.index = 0.0
         self.state = State.IDLE
         self.image = self.images[self.state][int (self.index)]
@@ -65,6 +66,7 @@ class PlayerSprite (pygame.sprite.Sprite):
         self.is_jumping         = False
         self.x_direction        = Direction.RIGHT
         self.y_direction        = Direction.DOWN
+        self.score              = 0
         self.velocity           = 0     # Velocity for each tick
         self.acceleration       = 1     # Acceleration due to gravity
         self.gravity_acc        = 1
@@ -132,9 +134,12 @@ class PlayerSprite (pygame.sprite.Sprite):
             self.rect.bottom = spike_sprite.rect.top
             self.dead ()
 
-        for zombie_sprite in pygame.sprite.spritecollide (self, self.zombie_group, False, pygame.sprite.collide_rect_ratio(0.65)):
+        for zombie_sprite in pygame.sprite.spritecollide (self, self.zombie_group, False, pygame.sprite.collide_rect_ratio(0.7)):
             self.dead ()
 
+        for coin_sprite in pygame.sprite.spritecollide (self, self.coin_group, True):
+            self.score += 1
+            print ("Grabbed a coin, score : " + str (self.score))
         for collided_sprite in pygame.sprite.spritecollide (self, self.platform_group, False):
             # We need to check if player is in the top bottom range so that we can either clamp
             # the jump or consider the top as end of jump
@@ -163,11 +168,30 @@ class PlayerSprite (pygame.sprite.Sprite):
         #printRect ("After collision player rect:", self.rect)
 ################################################################################################
 
+    def walk (self, direction):
+        self.x_direction = direction
+        self.state = State.WALK
+        speed = WALK_SPEED
+        if direction == Direction.LEFT:
+            speed = -WALK_SPEED
+        self.move (speed, 0)
+
+    def run (self, direction):
+        self.x_direction = direction
+        self.state = State.RUN
+        speed = RUN_SPEED
+        if direction == Direction.LEFT:
+            speed = -RUN_SPEED
+        self.move (speed, 0)
+
     def move (self, dx=0, dy=0):
         if not self.is_dead:
             #print (str (self.state) + " INSIDE Update " + str (self.x_direction))
             self.rect.x += dx
             self.rect.y += dy
+
+    def is_alive (self):
+        return not self.is_dead
 
     def clamp_rect (self):
         if self.rect.right >= WIDTH:
@@ -276,26 +300,39 @@ class SpikeSprite (pygame.sprite.Sprite):
         self.rect.x = rect.x
         self.rect.y = rect.y
 
-def main ():
-    pygame.init ()
+class CoinSprite (pygame.sprite.Sprite):
+    def __init__ (self, rect):
+        super (CoinSprite, self).__init__ ()
+        self.image = pygame.Surface([rect.w, rect.h])
+        self.image.fill (pygame.Color ("gold"))
+        self.rect = self.image.get_rect ()
+        self.rect.x = rect.x
+        self.rect.y = rect.y
 
-    screen = pygame.display.set_mode (SIZE)
-    screen_rect = screen.get_rect ()
+class GameLoop ():
+    def __init__ (self):
+        pygame.init ()
 
-    ground1 = PlatformSprite (pygame.Rect (0, 200, 100, 30), "black")
-    ground2 = PlatformSprite (pygame.Rect (500, 400, 100, 30), "black")
-    ground3 = PlatformSprite (pygame.Rect (0, 500, 300, 30), "black")
-    spike  = SpikeSprite (pygame.Rect (0, 650, WIDTH, 50), "red")
-    platforms = pygame.sprite.Group (ground1, ground2, ground3)
-    spikes  = pygame.sprite.Group (spike)
-    zombie = ZombieSprite (pygame.Rect (0, 470, 300, 30))
-    zombies = pygame.sprite.Group (zombie)
-    player = PlayerSprite ((100, 200), platforms, spikes, zombies, screen_rect)
-    actors = pygame.sprite.Group (player, zombie)
+        self.screen = pygame.display.set_mode (SIZE)
+        self.screen_rect = self.screen.get_rect ()
+        self.clock = pygame.time.Clock ()
 
-    clock = pygame.time.Clock ()
+    def init_level (self, level=None):
+        if level is None:
+            ground1 = PlatformSprite (pygame.Rect (900, 500, 100, 30), "black")
+            ground2 = PlatformSprite (pygame.Rect (500, 400, 100, 80), "black")
+            ground3 = PlatformSprite (pygame.Rect (0, 500, 300, 30), "black")
+            coin    = CoinSprite (pygame.Rect (920, 450, 30, 40))
+            spike  = SpikeSprite (pygame.Rect (0, 650, WIDTH, 50), "red")
+            zombie = ZombieSprite (pygame.Rect (0, 470, 300, 30))
+            self.coins = pygame.sprite.Group (coin)
+            self.spikes  = pygame.sprite.Group (spike)
+            self.platforms = pygame.sprite.Group (ground1, ground2, ground3)
+            self.zombies = pygame.sprite.Group (zombie)
+            self.player = PlayerSprite ((100, 200), self.platforms, self.spikes, self.zombies, self.coins, self.screen_rect)
+            self.actors = pygame.sprite.Group (self.player, zombie, coin)
 
-    while True:
+    def get_events (self):
         for event in pygame.event.get ():
             if (event.type == pygame.KEYDOWN and event.key == pygame.K_q) or \
                 event.type == pygame.QUIT:
@@ -303,47 +340,43 @@ def main ():
                 pygame.quit ()
                 quit ()
             if (event.type == pygame.KEYDOWN and event.key == pygame.K_d):
-                player.dead ()
+                self.player.dead ()
                 break
             if (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
-                player.jump ()
+                self.player.jump ()
                 #print ("Jumping here " + str (int (Direction.UP)))
 
-        speed = 0
-        if player.state != State.DEAD:
+        if self.player.is_alive ():
             keys = pygame.key.get_pressed ()
             if keys[pygame.K_LEFT]:
-                player.x_direction = Direction.LEFT
-                player.state = State.WALK
-                speed = -WALK_SPEED
                 if pygame.key.get_mods () & pygame.KMOD_SHIFT:
-                    speed = -RUN_SPEED
-                    player.state = State.RUN
-                #player.move (speed, 0)
+                    self.player.walk (Direction.LEFT)
+                else:
+                    self.player.run (Direction.LEFT)
             elif keys[pygame.K_RIGHT]:
-                player.x_direction = Direction.RIGHT
-                player.state = State.WALK
-                speed = WALK_SPEED
                 if pygame.key.get_mods () & pygame.KMOD_SHIFT:
-                    speed = RUN_SPEED
-                    player.state = State.RUN
-                #player.move (speed, 0)
+                    self.player.walk (Direction.RIGHT)
+                else:
+                    self.player.run (Direction.RIGHT)
 
-        player.move (speed, 0)
+    def update_frame (self):
+        self.screen.fill (BG_COLOR)
 
-        screen.fill (BG_COLOR)
+        self.actors.update ()
 
-        actors.update ()
-
-        actors.draw (screen)
-        platforms.draw (screen)
-        spikes.draw (screen)
+        self.actors.draw (self.screen)
+        self.platforms.draw (self.screen)
+        self.spikes.draw (self.screen)
 
         pygame.display.update ()
 
-        clock.tick (FPS)
+        self.clock.tick (FPS)
 
 
 if __name__ == '__main__':
     print ("Start")
-    main ()
+    game = GameLoop ()
+    game.init_level ()
+    while True:
+        game.get_events ()
+        game.update_frame ()
